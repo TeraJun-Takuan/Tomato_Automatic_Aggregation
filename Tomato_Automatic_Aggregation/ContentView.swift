e5abf2d0ff10e2d4eecf7522f12059d285a8942a
TeraJun <teraken_terao@icloud.com>
2021-06-02 15:12:35 +0900

    Initial Commit

---
+++ Tomato_Automatic_Aggregation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tomato_Automatic_Aggregation
+//
+//  Created by Terao Jumpei on 2021/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
