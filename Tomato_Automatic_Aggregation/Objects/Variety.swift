f23c02263e3c418d64337e6ede7e9a18b872c63f
TeraJun <teraken_terao@icloud.com>
2021-06-03 22:08:12 +0900

    品種IDと品種名を関連づける配列を削除

--- Tomato_Automatic_Aggregation/Objects/Variety.swift
+++ Tomato_Automatic_Aggregation/Objects/Variety.swift
@@ -10,11 +10,5 @@ import Foundation
 class Variety: ObservableObject {
     @Published var InputVarietyID: String = ""
     @Published var InputVarietyName: String = ""
-    
-    class VarietiesDataBase: Codable {
-        var id: String
-        var name: String
-    }
-    @Published var Varieties: [VarietiesDataBase] = Bundle.main.decodeJSON("varieties.json")
 }
 
